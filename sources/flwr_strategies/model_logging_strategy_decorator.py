98911d4b52ac31f12a1bdd77620ce8d6e7909742
M4rukku <57199511+M4rukku@users.noreply.github.com>
2021-12-14 22:33:28 +0100

    Added Predicate to the ModelLoggingStrategyDecorator
    I realised that far too many models got saved with the current decorator.
    Therefore, I decided to add a predicate which will decide if the current
    round should get saved.

--- sources/flwr_strategies/model_logging_strategy_decorator.py
+++ sources/flwr_strategies/model_logging_strategy_decorator.py
@@ -11,14 +11,21 @@ from sources.flwr_strategies.base_strategy_decorator import \
     BaseStrategyDecorator
 
 
+def log_every_tenth_round(rnd: int):
+    return rnd % 10 == 0
+
+
 class ModelLoggingStrategyDecorator(BaseStrategyDecorator):
 
     def __init__(self, strategy: flwr.server.strategy.Strategy,
                  model_saving_folder: PathLike,
-                 experiment_identifier: str):
+                 experiment_identifier: str,
+                 round_predicate=log_every_tenth_round
+                 ):
         super().__init__(strategy)
         self.model_saving_folder = model_saving_folder
         self.experiment_identifier = experiment_identifier
+        self.round_predicate = round_predicate
 
     def aggregate_fit(self,
                       rnd: int,
@@ -28,11 +35,15 @@ class ModelLoggingStrategyDecorator(BaseStrategyDecorator):
                 Tuple[Optional[Parameters], Dict[str, Scalar]],
                 Optional[Weights],  # Deprecated
             ]:
-        aggregated_weights = self.strategy.aggregate_fit(rnd, results, failures)
-        filename = \
-            create_round_based_model_saving_filename(rnd,
-                                                     self.model_saving_folder,
-                                                     self.experiment_identifier)
-        npz_parameters_to_file(filename,
-                               aggregated_weights)
+        aggregated_weights = \
+            self.strategy.aggregate_fit(rnd, results, failures)
+
+        if self.round_predicate(rnd):
+            filename = \
+                create_round_based_model_saving_filename(rnd,
+                                                         self.model_saving_folder,
+                                                         self.experiment_identifier)
+
+            npz_parameters_to_file(filename, aggregated_weights)
+
         return aggregated_weights
