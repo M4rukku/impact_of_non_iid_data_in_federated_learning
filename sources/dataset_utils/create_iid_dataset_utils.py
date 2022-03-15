9782740b42fb34a276095b3967a8c12bab95f17c
M4rukku <mw784@cam.ac.uk>
2022-03-15 01:17:31 +0100

    Fixed create_iid_dataset_utils
    The Script did not work for some corner cases (Lists of Images), which is now fixed.

--- sources/dataset_utils/create_iid_dataset_utils.py
+++ sources/dataset_utils/create_iid_dataset_utils.py
@@ -23,13 +23,28 @@ def extract_data(dataset_path: Path):
 
 def append_data(train_data, test_data, validation_data,
                 new_train_data, new_test_data, new_validation_data):
-    train_data["x"] = np.concatenate([train_data["x"], new_train_data["x"]])
-    train_data["y"] = np.concatenate([train_data["y"], new_train_data["y"]])
-    test_data["x"] = np.concatenate([test_data["x"], new_test_data["x"]])
-    test_data["y"] = np.concatenate([test_data["y"], new_test_data["y"]])
-    validation_data["x"] = np.concatenate([validation_data["x"], new_validation_data["x"]])
-    validation_data["y"] = np.concatenate([validation_data["y"], new_validation_data["y"]])
+    if isinstance(train_data["x"], list) and isinstance(new_train_data["x"], list):
+        train_data["x"] = train_data["x"] + new_train_data["x"]
+        train_data["y"] = train_data["y"] + new_train_data["y"]
+        test_data["x"] = test_data["x"] + new_test_data["x"]
+        test_data["y"] = test_data["y"] + new_test_data["y"]
+        validation_data["x"] = validation_data["x"] + new_validation_data["x"]
+        validation_data["y"] = validation_data["y"] + new_validation_data["y"]
+    else:
+        train_data["x"] = np.concatenate([train_data["x"], new_train_data["x"]])
+        train_data["y"] = np.concatenate([train_data["y"], new_train_data["y"]])
+        test_data["x"] = np.concatenate([test_data["x"], new_test_data["x"]])
+        test_data["y"] = np.concatenate([test_data["y"], new_test_data["y"]])
+        validation_data["x"] = np.concatenate([validation_data["x"], new_validation_data["x"]])
+        validation_data["y"] = np.concatenate([validation_data["y"], new_validation_data["y"]])
 
+
+def dataset_to_numpy_arrays(dataset: Dataset) -> Dataset:
+    return Dataset(
+        {"x": np.array(dataset.train["x"]), "y": np.array(dataset.train["y"])},
+        {"x": np.array(dataset.test["x"]), "y": np.array(dataset.test["y"])},
+        {"x": np.array(dataset.validation["x"]), "y": np.array(dataset.validation["y"])},
+    )
 
 def create_iid_dataset_from_client_fraction(base_data_dir: Path,
                                             dataset_identifier: str,
@@ -51,14 +66,20 @@ def create_iid_dataset_from_client_fraction(base_data_dir: Path,
     dataset = extract_data(dataset_selection[0])
 
     def empty_x():
-        shape_x = dataset[0]["x"].shape
+        arr = dataset[0]["x"]
+        if isinstance(arr, list):
+            return []
+        shape_x = arr.shape
         shape_x = (0, *(shape_x[1:]))
-        return np.empty(shape_x, dataset[0]["x"].dtype)
+        return np.empty(shape_x, arr.dtype)
 
     def empty_y():
-        shape_y = dataset[0]["y"].shape
+        arr = dataset[0]["y"]
+        if isinstance(arr, list):
+            return []
+        shape_y = arr.shape
         shape_y = (0, *(shape_y[1:]))
-        return np.empty(shape_y, dataset[0]["y"].dtype)
+        return np.empty(shape_y, arr.dtype)
 
     train_test_validation_dataset = Dataset({"x": empty_x(), "y": empty_y()},
                                             {"x": empty_x(), "y": empty_y()},
@@ -72,6 +93,8 @@ def create_iid_dataset_from_client_fraction(base_data_dir: Path,
                         {"x": empty_x(), "y": empty_y()})
         else:
             append_data(*train_test_validation_dataset, *extracted_data)
+
+    train_test_validation_dataset = dataset_to_numpy_arrays(train_test_validation_dataset)
 
     if compiled_dataset_name is None:
         iid_data_file = base_data_dir / \
@@ -101,14 +124,20 @@ def subsample_full_iid_datasets(base_data_dir: Path,
 
     if only_create_and_use_training_data:
         def empty_x():
-            shape_x = full_dataset.train["x"].shape
+            arr = full_dataset.train["x"]
+            if isinstance(arr, list):
+                return []
+            shape_x = arr.shape
             shape_x = (0, *(shape_x[1:]))
-            return np.empty(shape_x, full_dataset.train["x"].dtype)
+            return np.empty(shape_x, arr.dtype)
 
         def empty_y():
-            shape_y = full_dataset.train["y"].shape
+            arr = full_dataset.train["y"]
+            if isinstance(arr, list):
+                return []
+            shape_y = arr.shape
             shape_y = (0, *(shape_y[1:]))
-            return np.empty(shape_y, full_dataset.train["y"].dtype)
+            return np.empty(shape_y, arr.dtype)
 
         train_test_validation_dataset = Dataset({"x": full_dataset.train["x"][selection_train],
                                                  "y": full_dataset.train["y"][selection_train]},
@@ -119,8 +148,10 @@ def subsample_full_iid_datasets(base_data_dir: Path,
                                                  "y": full_dataset.train["y"][selection_train]},
                                                 {"x": full_dataset.test["x"][selection_test],
                                                  "y": full_dataset.test["y"][selection_test]},
-                                                {"x": full_dataset.validation["x"][selection_validation],
-                                                 "y": full_dataset.validation["y"][selection_validation]})
+                                                {"x": full_dataset.validation["x"][
+                                                    selection_validation],
+                                                 "y": full_dataset.validation["y"][
+                                                     selection_validation]})
 
     dataset_file = base_data_dir / compiled_dataset_name
     with dataset_file.open("wb") as f:
