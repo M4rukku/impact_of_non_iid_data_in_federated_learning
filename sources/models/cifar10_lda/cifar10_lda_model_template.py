5270aa01857bf6e01d3793e63a0feafe7ad11574
M4rukku <mw784@cam.ac.uk>
2022-03-21 08:42:02 +0100

    Changed default learning rate of Cifar10

--- sources/models/cifar10_lda/cifar10_lda_model_template.py
+++ sources/models/cifar10_lda/cifar10_lda_model_template.py
@@ -44,7 +44,7 @@ class Cifar10LdaModelTemplate(ModelTemplate):
     def get_centralised_metrics(self) -> List[Union[str, tf.keras.metrics.Metric]]:
         return get_default_sparse_categorical_metrics(self.num_classes)
 
-    def get_optimizer(self, lr=0.01, momentum=0.9, model: Optional[tf.keras.models.Model] = None) \
+    def get_optimizer(self, lr=0.1, momentum=0.9, model: Optional[tf.keras.models.Model] = None) \
             -> tf.keras.optimizers.Optimizer:
         if self.optimizer is not None:
             return self.optimizer
